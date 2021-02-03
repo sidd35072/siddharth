95f623aa466f37699058258685dd7961ddef9db4
Siddharth Rai <sidd35072@gmail.com>
2021-02-04 00:56:10 +0530

    Previous design complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+///////////////////////////////////////
+//Writing animation 
 const texts = ["Developer", "Designer", "Blogger", "Public Speaker"];
 var count = 1;
 var index = 0;
@@ -21,6 +23,8 @@ function type() {
 
 type();
 
+///////////////////////////////////////
+//Navigation
 
 const burger = document.querySelector(".navigation-burger");
 const nav = document.querySelector(".navigation");
@@ -47,3 +51,45 @@ document.querySelector("body").addEventListener('scroll', (e)=>{
   console.log(e);
   nav.classList.remove("expand");
 });
+
+
+///////////////////////////////////////
+//Animations
+
+
+function animate() {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll(".hidden");
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().y;
+      console.log(positionFromTop);
+
+      if (positionFromTop <= windowHeight / 1.3) {
+        console.log(element.classList);
+        if (element.classList.contains("aleft")) {
+          element.classList.add("fade-in-left");
+        } else if (element.classList.contains("aright")) {
+          element.classList.add("fade-in-right");
+        } else if (element.classList.contains("abottom")) {
+          element.classList.add("fade-in-bottom");
+        } else element.classList.add("fade-in-element");
+        element.classList.remove("hidden");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", checkPosition);
+  window.addEventListener("resize", init);
+
+  init();
+  checkPosition();
+}
+animate();
